fc01d207293f300e525bd4e7c6c29db097df1273
p-alvarenga <p.alvarenga.afonso@gmail.com>
2025-01-30 20:32:54 -0300

    simple commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 
-#define N 8 // Matrix Size [N x N] 
-#define C_EMPTY  "."
+#define N 8 // Matrix Size [N x N]
+#define C_EMPTY "."
 
 #include "./Knight.h"
 #include "./Pos.h"
@@ -11,66 +11,57 @@
 #define INIT_Y 0
 
 void printMatrix(short int matrix[N][N]);
-bool solveTour(Knight &k, int it, short int matrix[N][N]); 
+bool solveTour(Knight &k, int it, short int matrix[N][N]);
 bool availableMove(Pos p, short int matrix[N][N]);
 
-int main (void) {
+int main(void) {
+	short int matrix[N][N] = { 0 };
+
 	Knight k;
-	short int matrix[N][N];
-
 	k.pos.set(INIT_X, INIT_Y);
-
-	for (int i = 0; i < N; i++) 
-		for (int j = 0; j < N; j++)
-			matrix[i][j] = 0;
-
+	
 	matrix[INIT_X][INIT_Y] = 1;
 
-	clock_t begin = clock(); 
-	
+	clock_t begin = clock();
 	solveTour(k, 2, matrix);
-	
 	clock_t end = clock();
 
 	printMatrix(matrix);
-	printf("Time spent: %f", (double)(end - begin)/CLOCKS_PER_SEC); 
+	printf("Time spent: %f", (double)(end - begin) / CLOCKS_PER_SEC);
 
 	return 0;
-
 }
 
 bool solveTour(Knight &k, int it, short int matrix[N][N]) {
 	Pos p;
-	Knight k_jump; 
+	Knight k_jump;
 
-	if (it == 65) {
-		printMatrix(matrix);
-
+	if (it == N*N + 1) {
 		return 1;
 	}
 
 	for (int i = 0; i < N; i++) {
 		p = k.move(i);
 
-		if (p.validate() && matrix[p.x][p.y] == 0) {
-			//matrix[k.pos.x][k.pos.y] = it - 1; 
-			matrix[p.x][p.y] = it;
-			k_jump.pos = p;
-	
-			if (solveTour(k_jump, it + 1, matrix)) {
-				//printMatrix(matrix);
-		
-				return 1;
-			} else {
-				matrix[k_jump.pos.x][k_jump.pos.y] = 0;
-				i++;
-			}
+    if (p.validate() && matrix[p.x][p.y] == 0) {
+      // matrix[k.pos.x][k.pos.y] = it - 1;
+		matrix[p.x][p.y] = it;
+		k_jump.pos = p;
+
+		if (solveTour(k_jump, it + 1, matrix)) {
+        // printMatrix(matrix);
+
+			return 1;
+		} else {
+			matrix[k_jump.pos.x][k_jump.pos.y] = 0;
+			i++;
 		}
-	}
+    }
+  }
 
-	//printMatrix(matrix);
+  // printMatrix(matrix);
 
-	return 0; 
+  return 0;
 }
 
 void printMatrix(short int matrix[N][N]) {
@@ -78,23 +69,23 @@ void printMatrix(short int matrix[N][N]) {
 		for (int j = 0; j < N; j++) {
 			matrix[i][j] > 9 ? printf(" ") : printf("  ");
 
-			//matrix[i][j] != 0 ? printf("%i", matrix[i][j]) : printf("-");
+      // matrix[i][j] != 0 ? printf("%i", matrix[i][j]) : printf("-");
 
 			switch (matrix[i][j]) {
-				case 0: 
-					printf(C_EMPTY);
-					break;
-				case 99: 
-					printf(" #"); 
-					break;
-				default: 
-					printf("%i", matrix[i][j]); 
-					break;
+			case 0:
+				printf(C_EMPTY);
+				break;
+			case 99:
+				printf(" #");
+				break;
+			default:
+				printf("%i", matrix[i][j]);
+				break;
 			}
 		}
-		printf("\n"); 
+		printf("\n");
 	}
-	printf("\n\n");	
-	
-	return; 
+	printf("\n\n");
+
+	return;
 }
